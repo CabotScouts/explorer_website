8b7e493f575d22a565cf11ef56f87c797cd4909a
Owen Jones <owen@owenjones.net>
2019-01-05 21:47:44 +0000

    Initial layout and pages

---
+++ resources/views/calendar.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+@section('content')
+<section class="calendar pad">
+<!-- Will eventually replace this with an native implementation, but this'll do for now... -->
+	<div class="container grid-lg">
+		 <iframe src="https://calendar.google.com/calendar/embed?title=%20&amp;showTz=0&amp;wkst=2&amp;bgcolor=%23f7f8f9&amp;src=lna0octklnma00thnlq8e2jq20%40group.calendar.google.com&amp;color=%23AB8B00&amp;src=mghnd321gnm02vs5361s2lq4e8%40group.calendar.google.com&amp;color=%232F6309&amp;src=h1rprkaids2hafhvq7u0tsn7k8%40group.calendar.google.com&amp;color=%2329527A&amp;src=d21a2un76qqb17pdjdk2ad4gks%40group.calendar.google.com&amp;color=%23182C57&amp;src=34brbi6cbjdo30a725ln6qusms%40group.calendar.google.com&amp;color=%23711616&amp;ctz=Europe%2FLondon" scrolling="no"></iframe>
+	</div>
+</section>
+@endsection
---
+++ resources/views/units.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.main')
+@section('content')
+<section class="units container grid-lg pad">
+	<div class="columns">
+		<div class="column col-8 col-md-12 hide-sm">
+			<iframe src="https://www.google.com/maps/d/embed?mid=1riftuTRxvy14w2F-BYw9wl8fXRnpFncc"></iframe>
+		</div>
+		<div class="column col-4 col-md-12">
+			<div class="columns">
+				<div class="column col-sm-12 col-md-6 col-12">
+					<a href="/units/brabazon/">
+						<div class="card">
+							<div class="card-header">
+								<div class="card-title">Brabazon</div>
+								<div class="card-subtitle">Thursdays</div>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div class="column col-sm-12 col-md-6 col-12">
+					<a href="/units/brabazon/">
+						<div class="card">
+							<div class="card-header">
+								<div class="card-title">Concorde</div>
+								<div class="card-subtitle">Tuesdays</div>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div class="column col-sm-12 col-md-6 col-12">
+					<a href="/units/brabazon/">
+						<div class="card">
+							<div class="card-header">
+								<div class="card-title">Pirates</div>
+								<div class="card-subtitle">Wednesdays</div>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div class="column col-sm-12 col-md-6 col-12">
+					<a href="/units/brabazon/">
+						<div class="card">
+							<div class="card-header">
+								<div class="card-title">Spaniorum</div>
+								<div class="card-subtitle">Mondays</div>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div class="column col-sm-12 col-md-6 col-12">
+					<a href="/units/brabazon/">
+						<div class="card">
+							<div class="card-header">
+								<div class="card-title">Spitfire</div>
+								<div class="card-subtitle">Fridays</div>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div class="column col-sm-12 col-md-6 col-12">
+					<a href="/units/brabazon/">
+						<div class="card">
+							<div class="card-header">
+								<div class="card-title">Steama</div>
+								<div class="card-subtitle">Wednesdays</div>
+							</div>
+						</div>
+					</a>
+				</div>
+
+				<div class="column col-sm-12 col-md-6 col-12">
+					<a href="/units/brabazon/">
+						<div class="card">
+							<div class="card-header">
+								<div class="card-title">Young Leaders</div>
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+@endsection
