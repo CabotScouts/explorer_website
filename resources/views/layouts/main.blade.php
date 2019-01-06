d66f2e7e35a61b11a6533fcccddf256a58669987
Owen Jones <owen@owenjones.net>
2019-01-06 21:19:37 +0000

    More titles

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<link rel="stylesheet" href="{{ asset('assets/main.css') }}">
-		<title>@hassection('title')@yield('title') - @endifCabot Explorer Scouts</title>
+		<title>@yield('title') - Cabot Explorer Scouts</title>
 	</head>
 
 	<body>
