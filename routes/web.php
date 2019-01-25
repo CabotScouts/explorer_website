564e25f84dfc4c92173ef0f5fe44a7e2c103e3ce
Owen Jones <owen@owenjones.net>
2019-01-25 01:00:49 +0000

    New routes

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Home
-Route::get('/', function () {
-    return view('frontpage');
-})->name('home');
-
-// Calendar
-Route::get('/calendar/', function () {
-    return view('calendar');
-})->name('calendar');
-
-// Units
-Route::get('/units/{unit?}', 'UnitController@showUnit')->name('units');
-
 Auth::routes();
+Route::view('/', 'frontpage')->name('frontpage');
+Route::view('/calendar/', 'calendar')->name('calendar');
 
-// Catch-all route - look for post with that name
-Route::any('/{page?}', 'PageController@showPage');
+Route::get('/units/', 'UnitController@index')->name('units');
+Route::get('/units/{unit}', 'UnitController@showUnit');
+
+// Catch-all route - find a page which matches or 404
+Route::any('/{page?}', 'PageController@showPage')->where('page', '[.*/]+');
