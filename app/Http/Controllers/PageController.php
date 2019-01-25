31a7b72edae45ad59ce4b6c3cb04f3b729fff055
Owen Jones <owen@owenjones.net>
2019-01-25 00:02:20 +0000

    forgot to rename

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class PageController extends Controller
 {
     /**
      * Create a new controller instance.
--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class UnitController extends Controller
 {
     /**
      * Create a new controller instance.
