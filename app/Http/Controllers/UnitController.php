f9a2fcdfa5f7a2336062662d4306863b9326085e
Owen Jones <owen@owenjones.net>
2019-01-25 02:17:49 +0000

    Pass units to view

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -9,11 +9,11 @@ class UnitController extends Controller
 {
 	public function index() {
 		$units = Unit::orderBy('day', 'asc')->get();
-		return view('unit.index', $units);
+		return view('unit.index', ['units' => $units]);
 	}
 
   public function showUnit($name) {
 		$unit = Unit::where('name', $name)->get();
-    return view('unit.show', $unit);
+    return view('unit.show', ['unit' => $unit]);
   }
 }
