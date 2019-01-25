1dd22bfc09ba488d052b77bd19373cc417d30eb4
Owen Jones <owen@owenjones.net>
2019-01-25 02:39:51 +0000

    fix 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('title', '404')
 @section('content')
-<section class="frontpage-wide">
+<section class="container grid-xl">
 	<h1>404</h1>
 </section>
 @endsection
