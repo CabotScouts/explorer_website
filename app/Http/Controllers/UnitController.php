c4cc54293fe2079bee7eb4c9568a469a2702e075
Owen Jones <owen@owenjones.net>
2019-01-25 02:05:16 +0000

    Updated Unit controller and views

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 class UnitController extends Controller
 {
 	public function index() {
-		return view('units');
+		$units = App\Unit::orderBy('day', 'asc')->get();
+		return view('unit.index');
 	}
 
-  public function showUnit($unit) {
-    return $unit;
+  public function showUnit($name) {
+		$unit = App\Unit::where('name', $name)->get();
+    return view('unit.show', $unit);
   }
 }
