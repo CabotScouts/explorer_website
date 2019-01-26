3fc41b6d91da141b5846dfc3bccf9ce927e52856
Owen Jones <owen@owenjones.net>
2019-01-26 13:51:11 +0000

    Main menu seeder, added separate seeders to main seed file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	$this->call([
+				MenuLinkSeeder::class,
+				CurrentUnitSeeder::class
+			]);
     }
 }
--- database/seeds/MenuLinkSeeder.php
+++ database/seeds/MenuLinkSeeder.php
@@ -11,6 +11,47 @@ class MenuLinkSeeder extends Seeder
      */
     public function run()
     {
-        //
+			// Main Menu (set = navbar)
+			DB::table("links")->insert([
+				'set'   => 'navbar',
+				'name'  => 'Home',
+				'url'   => '/',
+				'order' => 1
+			]);
+
+			DB::table("links")->insert([
+				'set'   => 'navbar',
+				'name'  => 'Calendar',
+				'url'   => '/calendar/',
+				'order' => 2
+			]);
+
+			DB::table("links")->insert([
+				'set'   => 'navbar',
+				'name'  => 'Units',
+				'url'   => '/units/',
+				'order' => 3
+			]);
+
+			DB::table("links")->insert([
+				'set'   => 'navbar',
+				'name'  => 'Top Awards',
+				'url'   => '/awards/',
+				'order' => 4
+			]);
+
+			DB::table("links")->insert([
+				'set'   => 'navbar',
+				'name'  => 'Joining',
+				'url'   => '/join/',
+				'order' => 5
+			]);
+
+			DB::table("links")->insert([
+				'set'   => 'navbar',
+				'name'  => 'Volunteer',
+				'url'   => '/join/adults/',
+				'order' => 6
+			]);
     }
 }
