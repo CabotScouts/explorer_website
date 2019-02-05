e63a81a6a4c3f8f474c17e4ec63844a6326b15a4
Owen Jones <owen@owenjones.net>
2019-02-05 21:18:46 +0000

    Added proper link calls

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -24,38 +24,17 @@
 		<div class="columns">
 			<div class="column col-lg-4 col-sm-12">
 				<h4>For Explorers</h4>
-				<ul>
-					<li><a href="">Top Awards</a></li>
-					<li><a href="">Young Leaders</a></li>
-					<li><a href="">Duke of Edinburgh Award</a></li>
-					<li><a href="">Ten Tors</a></li>
-					<li><a href="">Namaste Nepal</a></li>
-					<li><a href="">Gromit Network</a></li>
-				</ul>
+				<?php print(menu('front-explorers')); ?>
 			</div>
 
 			<div class="column col-lg-4 col-sm-12">
 				<h4>For Parents</h4>
-				<ul>
-					<li><a href="">Parents Guide to Explorers</a></li>
-					<li><a href="">Parents' Committee</a></li>
-					<li><a href="https://www.onlinescoutmanager.co.uk/parents/" target="_blank">My.Scout Login</a></li>
-					<li><a href="">Contact Us</a></li>
-					<li><a href="https://bristolcabotscouts.org.uk">Cabot Scout District</a></li>
-					<li><a href="https://scouts.org.uk">The Scouts</a></li>
-				</ul>
+				<?php print(menu('front-parents')); ?>
 			</div>
 
 			<div class="column col-lg-4 col-sm-12">
 				<h4>For Leaders</h4>
-				<ul>
-					<li><a href="">Post-14 Meetings</a></li>
-					<li><a href="">Activity Notifications</a></li>
-					<li><a href="">Nights Away Permits</a></li>
-					<li><a href="">Email Login</a></li>
-					<li><a href="">Leader Shared Drive</a></li>
-					<li><a href="https://www.onlinescoutmanager.co.uk/login.php" target="_blank">Online Scout Manager</a></li>
-				</ul>
+				<?php print(menu('front-leaders')); ?>
 			</div>
 		</div>
 	</div>
