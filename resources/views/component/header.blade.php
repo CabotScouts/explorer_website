84de9afa01cb4fb80a61979b2349bf3794edf53a
Owen Jones <owen@owenjones.net>
2019-03-21 23:47:31 +0000

    Header component now includes both type of header

--- resources/views/component/header.blade.php
+++ resources/views/component/header.blade.php
@@ -1,3 +1,4 @@
+@if(!isset($orientation) || $orientation == 'horizontal')
 <section class="page-header cover-horizontal" style="background: url('{{ $image }}'); background-size: cover;">
 	<div class="page-title">
 		<div class="container grid-lg">
@@ -9,3 +10,14 @@
 		</div>
 	</div>
 </section>
+@else
+<section class="page-header cover-vertical" style="background: url('{{ $image }}'); background-size: cover;">
+	<div class="container grid-lg">
+		<div class="columns">
+			<div class="column col-5 col-lg-5 col-md-8 col-xs-12">
+				{{ $slot }}
+			</div>
+		</div>
+	</div>
+</section>
+@endif
--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -1,22 +1,16 @@
 @extends('layouts.main')
 @section('content')
-<section class="page-header cover-vertical" style="background: url('/assets/img/expedition-opt.jpg'); background-size: cover;">
-	<div class="container grid-lg">
-		<div class="columns">
-			<div class="column col-5 col-lg-5 col-md-8 col-xs-12">
-				<div>
-					<h4>Ready for Adventure?</h4>
-				</div>
-				<div>
-					<p>Cabot Explorers is the Scouting section for 14 - 18 year olds in Cabot District, providing new experiences, adventure, and skills to the young people of North West Bristol.</p>
-				</div>
-				<div class="tagline">
-					<p>We prepare young people with Skills For Life.</p>
-				</div>
-			</div>
-		</div>
+@component('component.header', ['orientation' => 'vertical', 'image' => '/assets/img/expedition-opt.jpg'])
+	<div>
+		<h4>Ready for Adventure?</h4>
 	</div>
-</section>
+	<div>
+		<p>Cabot Explorers is the Scouting section for 14 - 18 year olds in Cabot District, providing new experiences, adventure, and skills to the young people of North West Bristol.</p>
+	</div>
+	<div class="tagline">
+		<p>We prepare young people with Skills For Life.</p>
+	</div>
+@endcomponent
 
 <section class="links pad space">
 	<div class="container grid-lg">
