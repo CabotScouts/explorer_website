f6bd980f3e6af1397613b18e92c6534a937d0b32
Owen Jones <owen@owenjones.net>
2019-07-11 16:41:46 +0100

    Initial opengraph component so pages can be shared better

---
+++ resources/views/component/opengraph.blade.php
@@ -0,0 +1,19 @@
+{{-- TODO: Add proper description columns to pages --}}
+{{-- TODO: Add post data type and structure (difference between a page and post?) --}}
+<meta property="og:url" content="{{ url()->current() }}" />
+@if(isset($page))
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="{{ $page->title }}" />
+	<meta property="og:description" content="{{ $page->body }}" />
+	@if($page->image)
+		<meta property="og:image" content="{{ asset($page->image) }}" />
+	@endif
+@elseif(isset($post))
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="{{ $post->title }}" />
+	<meta property="og:description" content="{{ $post->summary }}" />
+	@if($post->image)
+		<meta property="og:image" content="{{ asset($page->image) }}" />
+	@endif
+@endif
+<meta property="og:locale" content="en_GB"  />
