1da9d245f34e5c310e9b312d9f251e5bbd3a10c9
Owen Jones <owen@owenjones.net>
2019-07-11 17:39:09 +0100

    Unit pages are now routed as regular pages

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -10,12 +10,12 @@ class UnitController extends Controller
 {
 	public function index() {
 		$units = Unit::where('status', 1)->orderBy('name', 'asc')->get();
-		$page = Page::where('slug', ("units/"))->where('status', 1)->first();
+		$page = Page::where('slug', ("units"))->where('status', 1)->first();
 		return view('units', ['units' => $units, 'page' => $page]);
 	}
 
-  public function showUnit($name) {
-		$page = Page::where('slug', ("units/" . $name))->where('status', 1)->firstOrFail();
-		return view('page', ['page' => $page]);
-  }
+  // public function showUnit($name) {
+	// 	$page = Page::where('slug', ("units/" . $name))->where('status', 1)->firstOrFail();
+	// 	return view('page', ['page' => $page]);
+  // }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::view('/', 'frontpage')->name('frontpage');
 Route::view('/calendar/', 'calendar')->name('calendar');
 
 Route::get('/units/', 'UnitController@index')->name('units');
-Route::get('/units/{unit}', 'UnitController@showUnit');
+// Route::get('/units/{unit}', 'UnitController@showUnit');
 
 Route::post('/search/', 'PageController@searchPages');
 
