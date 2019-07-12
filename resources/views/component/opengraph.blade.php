2d2c2bb85f20183b7f7c33518e1336a5f7f0f40e
Owen Jones <owen@owenjones.net>
2019-07-12 04:07:22 +0100

    Image path change

--- resources/views/component/opengraph.blade.php
+++ resources/views/component/opengraph.blade.php
@@ -7,14 +7,14 @@
 	<meta property="og:title" content="{{ $page->title }}" />
 	<meta property="og:description" content="{{ $page->body }}" />
 	@if($page->image)
-		<meta property="og:image" content="{{ asset($page->image) }}" />
+		<meta property="og:image" content="{{ $page->image }}" />
 	@endif
 @elseif(isset($post))
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content="{{ $post->title }}" />
 	<meta property="og:description" content="{{ $post->summary }}" />
 	@if($post->image)
-		<meta property="og:image" content="{{ asset($page->image) }}" />
+		<meta property="og:image" content="{{ $page->image }}" />
 	@endif
 @endif
 <meta property="og:locale" content="en_GB"  />
