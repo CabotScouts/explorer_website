d9dc38579a08b9ee39449c707e7ee479d9553ac1
Owen Jones <owen@owenjones.net>
2019-08-26 02:56:20 +0100

    Removed seeders for old content

--- database/seeds/MainMenuSeeder.php
+++ database/seeds/MainMenuSeeder.php
@@ -18,36 +18,6 @@ class MainMenuSeeder extends Seeder
 					'title'   => 'Home',
 					'url'     => '/',
 					'order'   => 1
-				],
-				[
-					'menu_id' => $menu,
-					'title'   => 'Calendar',
-					'url'     => '/calendar/',
-					'order'   => 2
-				],
-				[
-					'menu_id' => $menu,
-					'title'   => 'Units',
-					'url'     => '/units/',
-					'order'   => 3
-				],
-				[
-					'menu_id' => $menu,
-					'title'   => 'Top Awards',
-					'url'     => '/awards/',
-					'order'   => 4
-				],
-				[
-					'menu_id' => $menu,
-					'title'   => 'Join Us',
-					'url'     => '/join/',
-					'order'   => 5
-				],
-				[
-					'menu_id' => $menu,
-					'title'   => 'Volunteer',
-					'url'     => '/join/volunteer/',
-					'order'   => 6
 				]
 			]);
     }
