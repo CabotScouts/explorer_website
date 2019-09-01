75342a7a92902c7328c110d71cd0a073997ce8bb
Owen Jones <owen@owenjones.net>
2019-09-01 23:07:02 +0100

    404 Fixed

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.main')
 @section('title', '404')
 @section('content')
-<section class="container container-fluid pad">
+<section class="container container-fluid content-center">
 	<div class="row justify-content-md-center">
 		<div class="col col-12 col-lg-6 text-center">
 			<h1>404 - Page Not Found</h1>
 			<p>The page you were looking for could not be found.</p>
-			<form class="empty-action input-group input-inline" method="post" action="/search/">
+			{{-- <form class="form-inline" method="post" action="/search/">
 				@csrf
-				<input class="form-input" name="search" type="text" placeholder="">
-	       <button class="btn btn-primary input-group-btn">Search</button>
-			</form>
+				<label class="sr-only" for="searchBox">Search for a page</label>
+				<input type="text" class="form-control mr-sm-2" name="search" id="searchBox" placeholder="">
+	      <button type="submit" class="btn btn-primary">Search</button>
+			</form> --}}
 		</div>
 	</div>
 </section>
