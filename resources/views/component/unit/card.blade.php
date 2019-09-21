2bb01ea4b8a3377254dc4f176b8b53211928c72c
Owen Jones <owen@owenjones.net>
2019-09-21 23:04:56 +0100

    Tweaked where times appear

--- resources/views/component/unit/card.blade.php
+++ resources/views/component/unit/card.blade.php
@@ -2,11 +2,16 @@
 	@include('component.unit.logo', ['unit' => $unit])
 	<div class="card-body">
 		<h5 class="card-title">{{ $unit->name }}</h5>
-	@if($unit->day > -1)
-		<h6 class="card-subtitle muted">{{ $unit->dayString }}s: {{ $unit->start_time }} - {{ $unit->end_time }} </h6>
-	@else
-		<h6 class="card-subtitle muted">&nbsp;</h6>
-	@endif
+		<h6 class="card-subtitle">
+			@if($unit->day > -1)
+				{{ $unit->dayString }}s
+			@else
+				&nbsp;
+			@endif
+			@if($unit->start_time)
+				<small class="muted">({{ $unit->start_time }} - {{ $unit->end_time }})</small>
+			@endif
+		</h6>
 	</div>
 	<div class="card-body">
 		<a href="{{ route('view-unit', ['name' => $unit->shortname ]) }}/" class="btn btn-success mb-2">See Unit</a>
