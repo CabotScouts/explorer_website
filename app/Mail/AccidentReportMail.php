f32ca4163adb511129ba6e05f354a623d3121fd1
Owen Jones <owen@owenjones.net>
2019-11-23 23:10:29 +0000

    Added email subject and fixed outgoing address

---
+++ app/Mail/AccidentReportMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\Request;
+use App\AccidentReport;
+
+class AccidentReportMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+		public $id;
+		public $report;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(AccidentReport $report, Request $request)
+    {
+        $this->id = $report->id;
+				$this->report = $request;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Cabot Explorers - Accident Report (#' . $this->id . ')')
+				->from('no-reply@cabotexplorers.org.uk')
+				->markdown('mail.accidentreport');
+    }
+}
