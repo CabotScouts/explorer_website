cade57c929186e8ba078fd37de71d951450b45c0
Owen Jones <owen@owenjones.net>
2019-11-23 19:33:25 +0000

    Updated error prompt message

--- resources/views/form/errors.blade.php
+++ resources/views/form/errors.blade.php
@@ -1,5 +1,5 @@
 <div class="alert alert-danger">
-	<strong>Please check you've correctly filled in the required information</strong>
+	<strong>Please check you've correctly filled out any required information</strong>
 	<ul>
 		@foreach($errors->all() as $error)
 			<li>{{ $error }}</li>
