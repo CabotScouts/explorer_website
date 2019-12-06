d50cc3237278b1b1f47b6d44e31f77a023e8f7b9
Owen Jones <owen@owenjones.net>
2019-12-06 22:17:49 +0000

    Updated calendar location - will make this into a dynamic page eventually...

--- resources/views/page/calendar.blade.php
+++ resources/views/page/calendar.blade.php
@@ -10,7 +10,7 @@
 			</div>
 
 			<div class="col col-12">
-				 <iframe src="https://calendar.google.com/calendar/embed?title=%20&amp;showTz=0&amp;wkst=2&amp;bgcolor=%23f7f8f9&amp;src=lna0octklnma00thnlq8e2jq20%40group.calendar.google.com&amp;color=%23AB8B00&amp;src=mghnd321gnm02vs5361s2lq4e8%40group.calendar.google.com&amp;color=%232F6309&amp;src=h1rprkaids2hafhvq7u0tsn7k8%40group.calendar.google.com&amp;color=%2329527A&amp;src=d21a2un76qqb17pdjdk2ad4gks%40group.calendar.google.com&amp;color=%23182C57&amp;src=34brbi6cbjdo30a725ln6qusms%40group.calendar.google.com&amp;color=%23711616&amp;ctz=Europe%2FLondon" scrolling="no"></iframe>
+				 <iframe src="https://calendar.google.com/calendar/embed?title=%20&amp;showTz=0&amp;wkst=2&amp;bgcolor=%23f7f8f9&amp;src=Y2Fib3RleHBsb3JlcnMub3JnLnVrXzFkbGk0dG11cWE4cXA1NHVhMWo5NXA1cDYwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=Y2Fib3RleHBsb3JlcnMub3JnLnVrXzUxODdsZXJidWtnYzA1b2JyYWo2Z3IyZTM4QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=Y2Fib3RleHBsb3JlcnMub3JnLnVrX3Blc3IzbXJlZzg4Mzd1ODVsMDdqY2RxYjcwQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;src=Y2Fib3RleHBsb3JlcnMub3JnLnVrX3BrbTFmN2M3dDZ0N2lqNGMwbnB2MWF1aTFrQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&amp;color=%23182C57&amp;src=34brbi6cbjdo30a725ln6qusms%40group.calendar.google.com&amp;color=%23711616&amp;ctz=Europe%2FLondon" scrolling="no"></iframe>
 			 </div>
 		</div>
 	</div>
