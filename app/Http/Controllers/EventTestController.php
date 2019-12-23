87c9498a65b6c271f37784d83a5485987446b1ec
Owen Jones <owen@owenjones.net>
2019-12-23 02:48:03 +0000

    A test controller for events

---
+++ app/Http/Controllers/EventTestController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\EventSource;
+use App\Calendar;
+use App\Event;
+
+class EventTestController extends Controller
+{
+  // test methods for creating/manipulating event related things
+}
