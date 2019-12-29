a33b893cbb697c3d14bdaa6b9ea027898e45780a
Owen Jones <owen@owenjones.net>
2019-12-29 13:47:55 +0000

    Fleshed out Calendar model - can now retrieve all events in calendar

--- app/Calendar.php
+++ app/Calendar.php
@@ -3,10 +3,41 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DateTime, DateInterval;
 
-class Calendar extends Model
-{
-    public function sources() {
-			return $this->belongsToMany('App\EventSource');
-		}
+class Calendar extends Model {
+  private $events = false;
+
+  public function sources() {
+		return $this->belongsToMany('App\EventSource');
+	}
+
+  public function events() {
+    if(!$this->events) $this->getEventsThrough();
+    return $this->events;
+  }
+
+  private function getEventsThrough() {
+    // laravel doesn't natively support hasManyThrough through a many-to-many relationship, so we need
+    // to work through each source and add it's events into a new collection
+    $this->events = collect();
+    foreach($this->sources()->get() as $source) {
+      $this->events = $this->events->concat($source->events()->get());
+    }
+
+    return true;
+  }
+
+  public function getMonth($month = false, $year = false) {
+    $year  = $year ? $year : (new DateTime)->format("Y");
+    $month = $month ? $month : (new DateTime)->format("m");
+    
+    $d = new DateTime;
+    $d->setDate($year, $month, 01);
+
+    $start = $d->format("Y-m-d");
+    $end = $d->add(new DateInterval("P1M"))->format("Y-m-d");
+
+    return $this->events()->whereBetween('start', [$start, $end]);
+  }
 }
