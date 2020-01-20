bca5e0bd667fe1eccef3108cfa2f8d47a477a8a8
Owen Jones <owen@owenjones.net>
2020-01-20 03:29:25 +0000

    tweak photo header margin

--- resources/views/instagram/index.blade.php
+++ resources/views/instagram/index.blade.php
@@ -4,11 +4,11 @@
 @if(isset($media))
 <section class="page container pad space">
   <div class="row">
-    <div class="col col-12 col-lg-10 mt-2 mb-3">
+    <div class="col col-12 col-lg-10 mt-1 mb-3">
       <h1>{{ $tag->formatted }}</h1>
     </div>
     <div class="col col-lg-2">
-      <div class="dropdown mt-3 mr-4 text-right">
+      <div class="dropdown mt-2 mr-4 text-right">
         <a class="navbar-link dropdown-toggle" href="#" id="albumsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All Albums</a>
 
       	<div class="dropdown-menu" aria-labelledby="albumsDropdown">
