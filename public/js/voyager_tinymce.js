9babd10a05976d10fbcf1d12334eea62107ec76e
Owen Jones <owen@owenjones.net>
2020-04-19 03:05:55 +0100

    enabled all tags in tinymce (can only be used by an admin anyway) to allow svgs to be used

--- public/js/voyager_tinymce.js
+++ public/js/voyager_tinymce.js
@@ -1,8 +1,7 @@
 function tinymce_setup_callback(editor)
 {
 	editor.settings['paste_as_text'] = true;
-
-  // edit TinyMCE table creator properties to play nice with Spectre
+	editor.settings['non_empty_elements'] += "svg,defs,pattern,desc,metadata,g,mask,path,line,marker,rect,circle,ellipse,polygon,polyline,linearGradient,radialGradient,stop,image,view,text,textPath,title,tspan,glyph,symbol,switch,use";
 	editor.settings['table_default_attibutes'] = {
 		border: '0',
 		class: 'table'
@@ -16,7 +15,7 @@ function tinymce_setup_callback(editor)
 		'tr' : 'height width border padding margin',
 		'table' : 'height width border padding margin'
 	};
-	editor.settings['extended_valid_elements'] = 'table[class=table|class=table-striped]';
+	editor.settings['extended_valid_elements'] = 'table[class=table|class=table-striped],*[*]';
 	editor.settings['table_resize_bars'] = false;
 	editor.settings['table_class_list'] = [
 		{ title: 'Normal', value: 'table' },
