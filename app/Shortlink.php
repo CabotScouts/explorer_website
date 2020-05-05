bf3e1d23dc4321a753af9bc59c23a3a72056065c
Owen Jones <owen@owenjones.net>
2020-05-05 18:43:06 +0100

    Initial Shortlink model

---
+++ app/Shortlink.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shortlink extends Model {
+    // Shortlink - a short 'code' which redirects to a saved link
+    // Codes can either be specified, or randomly generated
+
+    public function generateCode() {
+      
+    }
+}
