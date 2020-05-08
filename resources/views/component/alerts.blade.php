a4eabf0a987c327afb3cc8f163eb8d8a4af0c006
Owen Jones <owen@owenjones.net>
2020-05-08 01:01:10 +0100

    alerts can now take HTML

--- resources/views/component/alerts.blade.php
+++ resources/views/component/alerts.blade.php
@@ -5,7 +5,7 @@ $alerts = session('alert');
 @if($alerts)
 <section class="alerts container pad space">
   @foreach($alerts as $type => $message)
-  <div class="alert alert-{{ $type }}">{{ $message }}</div>
+  <div class="alert alert-{{ $type }}">{!! $message !!}</div>
   @endforeach
 </section>
 @endif
