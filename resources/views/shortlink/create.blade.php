e8f90d878263e759f3378423dbf205102fe840bc
Owen Jones <owen@owenjones.net>
2020-05-08 20:44:16 +0100

    display tweaks - last alert has no bottom margin, new shortlink form responsiveness

--- resources/views/shortlink/create.blade.php
+++ resources/views/shortlink/create.blade.php
@@ -8,7 +8,7 @@
       <h1>New Shortlink</h1>
     </div>
 
-    <div class="page-content col col-12 mt-3">
+    <div class="page-content col col-12">
       <form method="post" action="{{ route('shortlink.create.store') }}" novalidate>
 				@csrf
 
@@ -17,24 +17,24 @@
 				@endif
 
 				<div class="form-group row">
-					<label for="url" class="col-lg-6 col-sm-4 col-form-label">URL*</label>
-          <div class="col-lg-6 col-sm-8">
+					<label for="url" class="col-12 col-sm-2 col-form-label">URL</label>
+          <div class="col-12 col-sm-10">
             <input class="form-control" type="text" name="url" id="url" placeholder="https://" required />
           </div>
 				</div>
 
         <div class="form-group row">
-					<label for="code" class="col-lg-6 col-sm-4 col-form-label">Shortlink</label>
-          <div class="col-lg-6 col-sm-8">
-            <input class="form-control" type="text" name="code" id="code" placeholder="abcdef" />
+					<label for="code" class="col-12 col-sm-2 col-form-label">Shortlink</label>
+          <div class="col-12 col-sm-10">
+            <input class="form-control" type="text" name="code" id="code" />
           </div>
-          <div class="col-lg-6 col-sm-8 ml-auto">
+          <div class="col-12 col-sm-10 ml-auto">
             <small>Leave blank for a randomly generated code</small>
           </div>
 				</div>
 
         <div class="form-group row">
-          <div class="col col-lg-6 col-sm-8 ml-auto">
+          <div class="col-12 col-sm-10 ml-auto">
             <input class="form-control btn btn-outline-primary" type="submit" name="submit" value="Create Shortlink" />
           </div>
 				</div>
