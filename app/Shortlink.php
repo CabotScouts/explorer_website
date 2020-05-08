3a14f3f57f4242380a8ec10f6b3b0af91712a900
Owen Jones <owen@owenjones.net>
2020-05-08 14:28:31 +0100

    properly generates codes (and crudely checks if it's unique)

--- app/Shortlink.php
+++ app/Shortlink.php
@@ -3,12 +3,29 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Kyslik\ColumnSortable\Sortable;
 
 class Shortlink extends Model {
     // Shortlink - a short 'code' which redirects to a saved link
     // Codes can either be specified, or randomly generated
 
+    use Sortable;
+    public $sortable = [ 'id', 'code', 'url', 'clicks', 'created_at', 'updated_at' ];
+
     public function generateCode() {
-      
+      $len = config('shortlinks.length');
+      $generated = Str::random($len);
+      $check = Shortlink::where('code', $generated)->count();
+      return ($check === 0) ? $generated : $this->generateCode(); // risky...
+    }
+
+    public function registerClick() {
+      $this->clicks += 1;
+      $this->save();
+    }
+
+    public function getRedirectURLAttribute() {
+      return route('shortlink.redirect', ['code' => $this->code ]);
     }
 }
