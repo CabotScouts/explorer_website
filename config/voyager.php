a8892f02f2760f1ec671705708391c79cda16494
Owen Jones <owen@owenjones.net>
2020-12-07 18:46:39 +0000

    hide posts dimmer - makes voyager crash on the dashboard

--- config/voyager.php
+++ config/voyager.php
@@ -161,7 +161,7 @@ return [
 
         'widgets' => [
             'TCG\\Voyager\\Widgets\\UserDimmer',
-            'TCG\\Voyager\\Widgets\\PostDimmer',
+            // 'TCG\\Voyager\\Widgets\\PostDimmer',
             'TCG\\Voyager\\Widgets\\PageDimmer',
         ],
 
