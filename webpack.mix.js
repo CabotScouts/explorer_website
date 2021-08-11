c11781e281ce2fee130d413797ac812dd4db2ed3
Owen Jones <owen@owenjones.net>
2021-08-11 01:07:37 +0100

    package updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,2 @@
 const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.sass('resources/sass/app.scss', 'public/css').styles('node_modules/@fortawesome/fontawesome-free/css/all.css', 'public/css/icons.css');
