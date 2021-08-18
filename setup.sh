84a0faa6f399ebd58880f105ae1625d601b231cf
Owen Jones <owen@owenjones.net>
2021-08-18 18:44:57 +0100

    permissions fixes - change permissions here in prod. environment instead of in Dockerfile

--- setup.sh
+++ setup.sh
@@ -2,7 +2,12 @@
 source .env
 
 if [[ "$APP_ENV" = "production" ]]; then
+  chown -R www-data:www-data /data
+  chmod -R 775 ./storage/logs
+  chmod -R 775 ./bootstrap/cache
+  
   composer install --optimize-autoloader --no-dev
+  
   php artisan config:cache
   php artisan route:cache
   php artisan view:cache
