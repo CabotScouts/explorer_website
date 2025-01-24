ac96e5e34202ddd667aef4ef7273311449886ef9
Owen Jones <owen@owenjones.net>
2025-01-24 22:23:05 +0000

    fixed issue with xml opening tag being parsed as php

--- src/resources/views/sitemap.blade.php
+++ src/resources/views/sitemap.blade.php
@@ -1,15 +1,15 @@
-<?xml version="1.0" encoding="UTF-8"?>
+@php echo '<? xml version="1.0" encoding="UTF-8" ?>' @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  @foreach($pages as $page)
-   <url>
+  @foreach ($pages as $page)
+    <url>
       <loc>{{ url($page->slug) }}</loc>
-      <lastmod>{{ (new Carbon\Carbon($page->updated_at))->format("Y-m-d") }}</lastmod>
-   </url>
+      <lastmod>{{ (new Carbon\Carbon($page->updated_at))->format('Y-m-d') }}</lastmod>
+    </url>
   @endforeach
-  @foreach($units as $unit)
-   <url>
-      <loc>{{ url("units/" . $unit->shortname) }}</loc>
+  @foreach ($units as $unit)
+    <url>
+      <loc>{{ url('units/' . $unit->shortname) }}</loc>
       <changefreq>weekly</changefreq>
-   </url>
+    </url>
   @endforeach
 </urlset>
