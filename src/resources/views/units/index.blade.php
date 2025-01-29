9b72cd6072db97fc14be30d52d27479b0f64dab9
Owen Jones <owen@owenjones.net>
2025-01-29 21:29:15 +0000

    update unit maps to use latest Google Maps API

--- src/resources/views/units/index.blade.php
+++ src/resources/views/units/index.blade.php
@@ -1,106 +1,114 @@
 @extends('layouts.main')
 @section('title', 'Units')
 @section('content')
-<section class="wide-map">
-	<div id="map"></div>
-	<div class="map-overlay">
-		<div class="map-title">
-			<div class="container pad">
-				<div class="row">
-					<div class="col col-12 col-md-6 col-lg-8">
-						<h1>Explorer Units</h1>
-					</div>
-					{{-- <div class="actions col col-6 col-lg-4">
-						<a href="#" class="btn btn-primary">District Explorer Team</a>
-					</div> --}}
-				</div>
-			</div>
-		</div>
-	</div>
-	<script>
-	var unitmap = [];
+  <section class="wide-map">
+    <div id="map"></div>
+    <div class="map-overlay">
+      <div class="map-title">
+        <div class="container pad">
+          <div class="row">
+            <div class="col col-12 col-md-6 col-lg-8">
+              <h1>Explorer Units</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script>
+      var unitmap = [];
 
-	var markers = [
-@if(count($units) > 0)
-@foreach($units as $u)
-@if(($u->day > -1) && $u->lat)
-		{
-			name : "{{ $u->name }}",
-			day  : "{{ $u->dayString }}s",
-			lat  : {{ $u->lat }},
-			lng  : {{ $u->lng }},
-			url  : "{{ route('unit.view', $u->shortname) }}"
-		},
-@endif
-@endforeach
-@endif
-	];
+      var markers = [
+        @if (count($units) > 0)
+          @foreach ($units as $u)
+            @if ($u->day > -1 && $u->lat)
+              {
+                name: "{{ $u->name }}",
+                day: "{{ $u->dayString }}s",
+                lat: {{ $u->lat }},
+                lng: {{ $u->lng }},
+                url: "{{ route('unit.view', $u->shortname) }}"
+              },
+            @endif
+          @endforeach
+        @endif
+      ];
 
-	function addMarker(m) {
-		markers[m].label = new google.maps.InfoWindow({
-			content : `<a href="${ markers[m].url }"><strong>${ markers[m].name }</strong></a><br /><small>${ markers[m].day }</small>`
-		});
+      function addMarker(m) {
+        markers[m].label = new google.maps.InfoWindow({
+          content: `<a href="${ markers[m].url }"><strong>${ markers[m].name }</strong></a><br /><small>${ markers[m].day }</small>`
+        });
 
-		markers[m].marker = new google.maps.Marker({
-			position : { lat : markers[m].lat, lng : markers[m].lng },
-			map : unitmap,
-			animation: google.maps.Animation.DROP
-		});
+        markers[m].marker = new google.maps.marker.AdvancedMarkerElement({
+          position: {
+            lat: markers[m].lat,
+            lng: markers[m].lng
+          },
+          map: unitmap,
+        });
 
-		markers[m].marker.addListener('click', function() {
-			for(i = 0; i < markers.length; i++) {
-				markers[i].label.close();
-			}
-			markers[m].label.open(unitmap, markers[m].marker);
-		});
-	}
+        markers[m].marker.addListener('click', function() {
+          for (i = 0; i < markers.length; i++) {
+            markers[i].label.close();
+          }
+          markers[m].label.open(unitmap, markers[m].marker);
+        });
 
-	function initMap() {
-		unitmap = new google.maps.Map(
-			document.getElementById('map'), {
-				zoom: {{ config('explorers.map.zoom') }},
-				center: { lat: {{ config('explorers.map.centre.latitude') }}, lng: {{ config('explorers.map.centre.longitude') }} },
-				disableDefaultUI : true,
-				gestureHandling : 'cooperative',
-			});
+        markers[m].marker.content.classList.add("drop");
+      }
 
-		unitmap.addListener('click', function() {
-			for(i = 0; i < markers.length; i++) {
-				markers[i].label.close();
-			}
-		});
+      function initMap() {
+        unitmap = new google.maps.Map(
+          document.getElementById('map'), {
+            zoom: {{ config('explorers.map.zoom') }},
+            center: {
+              lat: {{ config('explorers.map.centre.latitude') }},
+              lng: {{ config('explorers.map.centre.longitude') }}
+            },
+            disableDefaultUI: true,
+            gestureHandling: 'cooperative',
+            mapId: 'unitMap',
+          });
 
-		for(var i = 0; i < markers.length; i++) {
-			var add = function(x) {
-				return function() {
-					addMarker(x);
-				}
-			}
-			setTimeout(add(i), i * 100);
-		}
-	}
-	</script>
-</section>
-<section class="page units container pad space">
-	<div class="row">
-		<div class="page-content col col-12 mt-2">
-				@if(isset($page))
-						{!! $page->body !!}
-				@endif
-		</div>
+        unitmap.addListener('click', function() {
+          for (i = 0; i < markers.length; i++) {
+            markers[i].label.close();
+          }
+        });
 
-		<div class="units-list col col-12">
-			<div class="row">
-				@if(count($units) > 0)
-					@foreach($units as $u)
-					<div class="col col-12 col-md-6 col-lg-3">
-						@include('component.unit.index-card', ['unit' => $u])
-					</div>
-					@endforeach
-				@endif
-			</div>
-		</div>
-	</div>
-</section>
-<script async defer src="https://maps.googleapis.com/maps/api/js?key={{ config('services.google.maps.key') }}&callback=initMap"></script>
+        for (var i = 0; i < markers.length; i++) {
+          var add = function(x) {
+            return function() {
+              addMarker(x);
+            }
+          }
+          setTimeout(add(i), i * 100);
+        }
+      }
+    </script>
+  </section>
+  <section class="page units container pad space">
+    <div class="row">
+      <div class="page-content col col-12 mt-2">
+        @if (isset($page))
+          {!! $page->body !!}
+        @endif
+      </div>
+
+      <div class="units-list col col-12">
+        <div class="row">
+          @if (count($units) > 0)
+            @foreach ($units as $u)
+              <div class="col col-12 col-md-6 col-lg-3">
+                @include('component.unit.index-card', ['unit' => $u])
+              </div>
+            @endforeach
+          @endif
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <script async
+    src="https://maps.googleapis.com/maps/api/js?key={{ config('services.google.maps.key') }}&loading=async&callback=initMap&libraries=marker">
+  </script>
 @endsection
--- src/resources/views/units/view.blade.php
+++ src/resources/views/units/view.blade.php
@@ -1,79 +1,88 @@
 @extends('layouts.main')
 @section('title', $unit->name)
 @section('content')
-@if($unit->hasMap)
-<section class="wide-map single-marker">
-	<div id="map"></div>
-	<div class="map-overlay">
-		<div class="map-title pad">
-			<div class="container">
-				<div class="row">
-					<div class="col col-12 col-md-6 col-lg-8">
-						@if(isset($root))
-						<h1>{{ $unit->name }}</h1>
-						@else
-						<h1>{{ $unit->name }} - {{ $page->title }}</h1>
-						@endif
-					</div>
-					<div class="actions col col-md-6 col-lg-4 d-none d-md-block">
-						@include('component.unit.unit-dropdown', ['units' => $units])
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<script>
-	function initMap() {
-		var unitmap = new google.maps.Map(
-			document.getElementById('map'), {
-				zoom: 15,
-				center: { lat: {{ $unit->lat }}, lng: {{ $unit->lng }} },
-				disableDefaultUI : true,
-				gestureHandling : 'cooperative',
-			});
+  @if ($unit->hasMap)
+    <section class="wide-map single-marker">
+      <div id="map"></div>
+      <div class="map-overlay">
+        <div class="map-title pad">
+          <div class="container">
+            <div class="row">
+              <div class="col col-12 col-md-6 col-lg-8">
+                @if (isset($root))
+                  <h1>{{ $unit->name }}</h1>
+                @else
+                  <h1>{{ $unit->name }} - {{ $page->title }}</h1>
+                @endif
+              </div>
+              <div class="actions col col-md-6 col-lg-4 d-none d-md-block">
+                @include('component.unit.unit-dropdown', ['units' => $units])
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script>
+        function initMap() {
+          var unitmap = new google.maps.Map(
+            document.getElementById('map'), {
+              zoom: 15,
+              center: {
+                lat: {{ $unit->lat }},
+                lng: {{ $unit->lng }}
+              },
+              disableDefaultUI: true,
+              gestureHandling: 'cooperative',
+              mapId: 'unitMap',
+            });
 
-		var marker = new google.maps.Marker({
-			position : { lat : {{ $unit->lat }}, lng : {{ $unit->lng }} },
-			map : unitmap
-		});
-	}
-	</script>
-</section>
-@else
-@include('component.breadcrumb', ['segments' => $page->breadcrumb])
-@endif
-<section class="page units pad space">
-	<div class="container">
-		<div class="row">
-			<div class="page-content col col-12 col-lg-9">
-			@if(!$unit->hasMap)
-				<h1>{{ $page->title }}</h1>
-			@endif
-			@if($page)
-				{!! $page->body !!}
-			@endif
-			@if(isset($media))
-			@include('instagram.media', ['media' => $media])
-			@endif
-			</div>
+          var marker = new google.maps.marker.AdvancedMarkerElement({
+            position: {
+              lat: {{ $unit->lat }},
+              lng: {{ $unit->lng }}
+            },
+            map: unitmap
+          });
+        }
+      </script>
+    </section>
+  @else
+    @include('component.breadcrumb', ['segments' => $page->breadcrumb])
+  @endif
+  <section class="page units pad space">
+    <div class="container">
+      <div class="row">
+        <div class="page-content col col-12 col-lg-9">
+          @if (!$unit->hasMap)
+            <h1>{{ $page->title }}</h1>
+          @endif
+          @if ($page)
+            {!! $page->body !!}
+          @endif
+          @if (isset($media))
+            @include('instagram.media', ['media' => $media])
+          @endif
+        </div>
 
-			<div class="page-sidebar col col-12 col-lg-3">
-				<div class="row">
-					<div class="block col col-12 col-md-4 col-lg-12">
-						@include('component.unit.view-card', [
-							'unit'  => $unit,
-							'units' => $units
-						])
-					</div>
+        <div class="page-sidebar col col-12 col-lg-3">
+          <div class="row">
+            <div class="block col col-12 col-md-4 col-lg-12">
+              @include('component.unit.view-card', [
+                  'unit' => $unit,
+                  'units' => $units,
+              ])
+            </div>
 
-				@if($page)
-					{!! $page->formattedSidebar !!}
-				@endif
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
+            @if ($page)
+              {!! $page->formattedSidebar !!}
+            @endif
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
-<script async defer src="https://maps.googleapis.com/maps/api/js?key={{ config('services.google.maps.key') }}&callback=initMap"></script>
+  <script async
+    src="https://maps.googleapis.com/maps/api/js?key={{ config('services.google.maps.key') }}&loading=async&callback=initMap&libraries=marker">
+  </script>
 @endsection
